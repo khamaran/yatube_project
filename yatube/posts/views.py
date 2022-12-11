c38387b385bb595f6e09cedcf63bf213ea03e938
Мария Тумилович <khamaran@yahoo.com>
2022-12-11 17:55:20 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Главная страница
+def index(request):    
+    return HttpResponse('Главная страница')
+
+
+# Страница, на которой будут посты, отфильтрованные по группам.
+def group_posts(request, slug):
+    return HttpResponse(f'На этой странице будут посты, отфильтрованные по группам {slug}.')
+
