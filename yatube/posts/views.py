882fa61744b1e349f2d72da0cd543f7792da47ce
Мария Тумилович <khamaran@yahoo.com>
2022-12-13 18:23:07 +0300

    До включения вёрстки с Практикума

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
-
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template) 
 
 # Страница, на которой будут посты, отфильтрованные по группам.
 def group_posts(request, slug):
